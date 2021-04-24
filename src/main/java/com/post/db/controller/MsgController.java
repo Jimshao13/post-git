2b618111e002d3b581baf9982fcc5d4552ec5f58
shaopengze <18805196853@163.com>
2021-04-24 17:54:20 +0800

    repair something

--- src/main/java/com/post/db/controller/MsgController.java
+++ src/main/java/com/post/db/controller/MsgController.java
@@ -67,7 +67,6 @@ public class MsgController {
 
 
     //对驿站所有未短信通知的快递发送短信通知,并修改相关快递短信状态为已通知
-    @ApiOperation(value="对驿站所有未短信通知的快递发送短信通知,并修改相关快递短信状态为已通知")
     @GetMapping("/informUninformed")
     public Map<String,Object> informUninformed(){
         List<Package> packs = packageService.findUninformed();
@@ -84,7 +83,9 @@ public class MsgController {
         querys.put("tpl_id", "TP2102014");
         Map<String, String> bodys = new HashMap<String, String>();
 
-        for(Package pack:packs){
+
+        //出于实际考虑，本部分目前做一个demo,将实际的代码先注释掉
+       /* for(Package pack:packs){
             querys.put("mobile",pack.getReceiverPhone());
             querys.put("param","code:"+pack.getPickUpCode());
             try {
@@ -97,6 +98,25 @@ public class MsgController {
                 e.printStackTrace();
             }
         }
+*/
+
+
+
+        querys.put("mobile","18805196853");
+        querys.put("param","code:"+"A-3");
+        try {
+
+            HttpResponse response = HttpUtils.doPost(host, path, method, headers, querys, bodys);
+            System.out.println(response.toString());
+            //获取response的body
+            //System.out.println(EntityUtils.toString(response.getEntity()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //修改未通知快递的通知状态为已通知
+        packageService.updateStatusInformed();
+
 
         Map<String,Object> map=new HashMap<>();
         try {
